9118d5e3ad60b296525a6fa6cfffaa118be39462
brenopereira18 <brenopereira@gmail.com>
2024-11-22 10:21:34 -0300

    :sparkles: feat: creates function with the last 10 transactions

--- transactions/viewSet/viewset.py
+++ transactions/viewSet/viewset.py
@@ -4,46 +4,12 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from transactions.models import Transaction
 from transactions.serializers import TransactionSerializer
-from django.db.models import Sum, DecimalField, F, ExpressionWrapper
+from .metrics import (calculate_amount_and_percentage, get_transactions_for_month, validate_transactions)
 
 
 class TransactionViewSet(viewsets.ModelViewSet):
   queryset = Transaction.objects.all()
   serializer_class = TransactionSerializer  
-
-  def get_transactions_for_month(self, month, year):
-    """Filtra as transações para o mês e ano passado."""
-    return self.queryset.filter(date__month=month, date__year=year)
-    
-  def validate_transactions(self, transactions):
-    """Verifica se há transações e se o total não é zero."""
-    total_transaction_amount = transactions.aggregate(total=Sum('amount'))['total']
-    if not total_transaction_amount or total_transaction_amount == 0:
-      return False, None
-    return True, total_transaction_amount
-    
-  def calculate_amount_and_percentage(self, transactions, total_transaction_amount, group_by_field):
-    """Calcula o total e a porcentagem por tipo de transação."""
-    transaction_data = (
-      transactions.values(group_by_field)  
-      .annotate(type_total=Sum('amount'))  
-      .annotate(
-        percentage=ExpressionWrapper(
-          F('type_total') * 100.0 / total_transaction_amount,
-          output_field=DecimalField()
-        )
-      )
-    )
-        
-    result = {
-      item[group_by_field]: {
-        'total': item['type_total'],
-        'percentage': round(item['percentage'], 2)  
-      }
-      for item in transaction_data
-    }
-        
-    return result
   
   def process_calculate(self, group_by_field, month=None, year=None, filter_by_type=None):
     """ Pega o mês e ano atual se não for passado. """
@@ -53,14 +19,14 @@ class TransactionViewSet(viewsets.ModelViewSet):
       year = current_date.year
       
     """ Obtem as transações para o mês e ano específico. """ 
-    transactions_for_month = self.get_transactions_for_month(month, year)
+    transactions_for_month = get_transactions_for_month(month, year)
 
     """ Filtra por transações do tipo EXPENSE. """
     if filter_by_type:
       transactions_for_month = transactions_for_month.filter(type=filter_by_type)
         
     """ Valida as transações. """ 
-    is_valid, total_transaction_amount = self.validate_transactions(transactions_for_month)
+    is_valid, total_transaction_amount = validate_transactions(transactions_for_month)
         
     if not is_valid:
       return Response(
@@ -69,10 +35,9 @@ class TransactionViewSet(viewsets.ModelViewSet):
       )
         
     """ Calcula o total e as porcentagens de trasações. """ 
-    result = self.calculate_amount_and_percentage(transactions_for_month, total_transaction_amount, group_by_field)
+    result = calculate_amount_and_percentage(transactions_for_month, total_transaction_amount, group_by_field)
         
     return Response(result)
-
     
   @action(detail=False, methods=['get'])
   def transaction_by_type(self, request, *args, **kwargs):
@@ -88,5 +53,22 @@ class TransactionViewSet(viewsets.ModelViewSet):
     year = request.query_params.get('year', None)
 
     return self.process_calculate('category', month, year, filter_by_type='EXPENSE')
+  
+  @action(detail=False, methods=['get'])
+  def last_transactions(self, request, *args, **kwargs):
+    month = request.query_params.get('month', None)
+    year = request.query_params.get('year', None)
+
+    if not month or not year:
+      current_date = datetime.now()
+      month = current_date.month
+      year = current_date.year
+
+    last_transactions = get_transactions_for_month(month, year)
+    response_last_transactions = last_transactions.order_by('-updatedAt')[:10]
+    
+    serializer = self.get_serializer(response_last_transactions, many=True)
+
+    return Response(serializer.data)
         
     
