cc1e6a2361d50345c41a841d504400b3e1608e26
brenopereira18 <brenopereira@gmail.com>
2024-11-23 23:31:28 -0300

    :recycle: refactor: change the name of the choices

--- transactions/models.py
+++ transactions/models.py
@@ -2,14 +2,13 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _  
 from django.contrib.auth.models import User
 
-
-TRANSACTION_TYPE = [
+TRANSACTION_TYPE_CHOICES = [
     ("DEPOSIT", _("Depósito")),  
     ("EXPENSE", _("Despesa")),
     ("INVESTMENT", _("Investimento")),
 ]
 
-TRANSACTION_CATEGORY = [
+TRANSACTION_CATEGORY_CHOICES = [
     ("HOUSING", _("Moradia")),  
     ("TRANSPORTATION", _("Transporte")),
     ("FOOD", _("Alimentação")),
@@ -21,7 +20,7 @@ TRANSACTION_CATEGORY = [
     ("OTHER", _("Outro")),
 ]
 
-PAYMENT_METHOD = [
+PAYMENT_METHOD_CHOICES = [
     ("CREDIT_CARD", _("Cartão de Crédito")),
     ("DEBIT_CARD", _("Cartão de Débito")),
     ("BANK_TRANSFER", _("Transferência Bancária")),
@@ -35,10 +34,10 @@ PAYMENT_METHOD = [
 class Transaction(models.Model):  
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_transaction")  
     name = models.CharField(max_length=50, verbose_name=_("Nome"))  
-    type = models.CharField(max_length=25, choices=TRANSACTION_TYPE, verbose_name=_("Tipo"))
+    type = models.CharField(max_length=25, choices=TRANSACTION_TYPE_CHOICES, verbose_name=_("Tipo"))
     amount = models.DecimalField(max_digits=8, decimal_places=2, verbose_name=_("Valor"))
-    category = models.CharField(max_length=25, choices=TRANSACTION_CATEGORY, verbose_name=_("Categoria"))
-    paymentMethod = models.CharField(max_length=25, choices=PAYMENT_METHOD, verbose_name=_("Método de Pagamento"))
+    category = models.CharField(max_length=25, choices=TRANSACTION_CATEGORY_CHOICES, verbose_name=_("Categoria"))
+    paymentMethod = models.CharField(max_length=25, choices=PAYMENT_METHOD_CHOICES, verbose_name=_("Método de Pagamento"))
     date = models.DateField(verbose_name=_("Data"))
     createdAt = models.DateTimeField(auto_now_add=True, verbose_name=_("Criado em"))
     updatedAt = models.DateTimeField(auto_now=True, verbose_name=_("Atualizado em"))
