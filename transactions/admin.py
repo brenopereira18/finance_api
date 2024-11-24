f9948bd74b9a8456f7d0bf3fe0df89bb75149ec9
brenopereira18 <brenopereira@gmail.com>
2024-11-24 12:15:01 -0300

    :recycle: refactor: adds user field

--- transactions/admin.py
+++ transactions/admin.py
@@ -3,7 +3,7 @@ from transactions.models import Transaction
 
 
 class TransactionAdmin(admin.ModelAdmin):
-  list_display = ('id', 'name', 'type', 'amount', 'category', 'paymentMethod', 'date',)
+  list_display = ('id', 'user', 'name', 'type', 'amount', 'category', 'paymentMethod', 'date',)
   fields = ('name', 'type', 'amount', 'category', 'paymentMethod', 'date',)
   search_fields =  ('name',)
 
