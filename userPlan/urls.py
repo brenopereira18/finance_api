c4ed8a82256498f6711de5c3da86dd4aba347514
brenopereira18 <brenopereira@gmail.com>
2024-11-26 18:39:31 -0300

    :sparkles: feat: adds route for stripe webhook

--- userPlan/urls.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from userPlan.views import UpdatePlanViewSet
-
-router = DefaultRouter()
-router.register(r'user-plan', UpdatePlanViewSet, basename='user-plan')
-urlpatterns = router.urls
